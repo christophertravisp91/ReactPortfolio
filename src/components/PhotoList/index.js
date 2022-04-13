2273fa05f068f606c7e9fd0a521341eec005dd84
TomFoolery <christophertravisp91@gmail.com>
2022-04-12 22:23:36 -0400

    fixing

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -8,25 +8,25 @@ const PhotoList = ({ category }) => {
   const [photos] = useState([
     {
       name: 'Resume',
-      category: 'portraits',
+      category: 'Resume',
       description:
         'Here is a current copy of my resume. If interested, request a copy and I will send it along with a cover letter.'
     },
     {
       name: 'Crescendo',
-      category: 'landscape',
+      category: 'Projects',
       description:
         'This was our 2nd project. It is a Music based community messaging app. Users can create topics based on any number of musical topics and interact with other users.'
     },
     {
       name: 'Drink and Draw',
-      category: 'landscape',
+      category: 'Projects',
       description:
         'Our 3rd project. It is a simple e-commerce site offering users options to order any number of our different packages of adult themed craft nights. It is deployed to heroku and has all elements of the MERN stack.'
     },
     {
       name: 'Weather Companion',
-      category: 'landscape',
+      category: 'Projects',
       description:
         'Our 1st project. It is a weather companion that pulls from 2 different APIs, weather, and a ticketmaster API, so users can plan events accordingly.'
     }
