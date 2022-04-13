f8d2e420df4a24c282a9f7c96aa58b24b6415824
TomFoolery <christophertravisp91@gmail.com>
2022-04-12 21:13:23 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   const [categories] = useState([
@@ -26,12 +27,13 @@ function App() {
       <main>
         {!contactSelected ? (
           <>
-            <About></About>
             <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
           </>
         ) : (
           <ContactForm></ContactForm>
         )}
+        <Footer></Footer>
       </main>
     </div>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = ({ onClose }) => {
+
+return (
+    <footer>
+    <div id="ContactInformation" class="ContactInformation"></div>
+    <h2>Contact Information</h2>
+    <ul>
+        <li>GitHub : <a href="https://github.com/christophertravisp91">My GitHub</a></li>
+        <li>Phone : (419) 591-6721</li>
+        <li><a href="https://www.linkedin.com/in/christopher-pierce-79412520b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BG%2B23UxyfTv%2BJBdt5NiURIw%3D%3D">LinkedIn</a></li>
+        <li>Email : christophertravisp91@gmail.com</li>
+        </ul>
+</footer>
+);
+};
+    
+
+export default Footer;
