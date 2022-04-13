b01682a9b65c7a707146a2716681777da58abe0e
TomFoolery <christophertravisp91@gmail.com>
2022-04-12 22:18:46 -0400

    fixing photos

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -12,12 +12,23 @@ const PhotoList = ({ category }) => {
       description:
         'Here is a current copy of my resume. If interested, request a copy and I will send it along with a cover letter.'
     },
-    
     {
-      name: 'Park bench',
+      name: 'Crescendo',
       category: 'landscape',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
+        'This was our 2nd project. It is a Music based community messaging app. Users can create topics based on any number of musical topics and interact with other users.'
+    },
+    {
+      name: 'Drink and Draw',
+      category: 'landscape',
+      description:
+        'Our 3rd project. It is a simple e-commerce site offering users options to order any number of our different packages of adult themed craft nights. It is deployed to heroku and has all elements of the MERN stack.'
+    },
+    {
+      name: 'Weather Companion',
+      category: 'landscape',
+      description:
+        'Our 1st project. It is a weather companion that pulls from 2 different APIs, weather, and a ticketmaster API, so users can plan events accordingly.'
     }
   ]);
 
